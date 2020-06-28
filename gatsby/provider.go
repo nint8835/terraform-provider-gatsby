6a32c2281933b87fa27bd4c6d9d78fa633347b6f
Riley Flynn <riley@rileyflynn.me>
2020-06-27 22:02:09 -0230

    Added provider base

---
+++ gatsby/provider.go
@@ -0,0 +1,9 @@
+package gatsby
+
+import ("github.com/hashicorp/terraform-plugin-sdk/helper/schema")
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{},
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nint8835/terraform-provider-gatsby
+
+go 1.14
+
+require github.com/hashicorp/terraform-plugin-sdk v1.14.0
